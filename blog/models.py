fb240fd8023db61190bddd901e6c70e66e520b3b
UMULISA12 <umulisaa0@gmail.com>
2019-04-17 12:38:13 +0200

    able to create a post as an dmin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author= models.CharField(max_length =60)
+    blog =models.ImageField(upload_to='gallery/')
+    title = models.CharField(max_length =200)
+    description = models.CharField(max_length =500)
+    date =models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+      return self.title
+
+    @classmethod
+    def get_posts(cls,post_id):
+        posts = Post.objects.get(id=post_id)
+        return posts     
+
+    @classmethod
+    def get_all_posts(cls):
+            posts = Post.objects.all()
+            return posts 
+        
+    
+    def save_posts(self):
+        self.save()
+
+    # def delete_images(self):
+    #   self.remove()
+    
+    # def update_images(self):
+    #   self.remove()
+    
+    # def get_image_by_id(id):
+    #   pass
+     
+    # def search_results(images):
+    #   pass
+    
+    # def filter_by_location(request,location_id):
+    #   pass
+    # class Meta:
+    #     ordering = ['images_name']
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def index(request):
   message = "welcome"
+  posts = Post.objects.all()
   return render(request, 'index.html')
+
+def blog(request):
+    posts = Post.objects.all()
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect("home")
+
+    else:
+        form = NewProfileForm()
+    return redirect("home")
+    return render(request, 'post.html',{'posts':posts})
